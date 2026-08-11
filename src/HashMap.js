a8bbccc7e1716cadcd1760f6240a8a26b103412a
Jesse <jessepullen@icloud.com>
2026-08-11 15:19:52 +1000

    Add and test set and get methods

--- src/HashMap.js
+++ src/HashMap.js
@@ -2,6 +2,8 @@ export default class HashMap {
 	constructor(loadFactor = 0.75, capacity = 16) {
 		this.loadFactor = loadFactor;
 		this.capacity = capacity;
+		// Creates an empty array as a bucket for number of capacity
+		this.buckets = Array.from({ length: capacity }, () => []);
 	}
 	// Hash function was imported with the inclusion of % capacity to keep hash within range of capacity
 	hash(key) {
@@ -13,6 +15,31 @@ export default class HashMap {
 		}
 
 		return hashCode;
+	}
+	set(key, value) {
+		const hash = this.hash(key);
+		const bucket = this.buckets[hash];
+
+		// Overwite value if key exists
+		for (let item of bucket) {
+			if (item[0] === key) {
+				item[1] = value;
+				return;
+			}
+		}
+
+		bucket.push([key, value]);
+	}
+	get(key) {
+		const hash = this.hash(key);
+		const bucket = this.buckets[hash];
+
+		for (let item of bucket) {
+			if (item[0] === key) {
+				return item[1];
+			}
+		}
+		return null;
 	}
 }
 
--- tests/HashMap.test.js
+++ tests/HashMap.test.js
@@ -1,9 +1,25 @@
 import HashMap from '../src/HashMap.js';
 
-test('Method takes a string key and produces a hash code within range of capacity', () => {
+test('Hash method takes a string key and produces a hash code within range of capacity', () => {
 	let hashMap = new HashMap(0.75, 16);
 
 	expect(hashMap.hash('cat')).toBe(6);
 	expect(hashMap.hash('hello')).toBe(2);
 	expect(hashMap.hash('apple')).toBe(10);
+});
+
+test('Set takes a key and a value assigned to the key, this should overwite existing key to new value', () => {
+	let hashMap = new HashMap(0.75, 16);
+
+	hashMap.set('Carlos', 'I am the old value.');
+	expect(hashMap.get('Carlos')).toBe('I am the old value.');
+
+	hashMap.set('Carlos', 'I am the new value.');
+	expect(hashMap.get('Carlos')).toBe('I am the new value.');
+});
+
+test('Get returns value of key or returns null', () => {
+	let hashMap = new HashMap(0.75, 16);
+
+	expect(hashMap.get('Test')).toBe(null);
 });
